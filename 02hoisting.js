ed0131560c6201146cef19f5cd3347055239c904
Ankush Thakur <atpara99@gmail.com>
2021-12-07 08:18:30 +0530

    Day 2 Completed

---
+++ 02hoisting.js
@@ -0,0 +1,20 @@
+getname2();
+getname3();
+
+console.log(a);
+
+console.log(getName);
+
+var a;
+
+function getName() {
+  console.log("JavaScript");
+}
+
+var getname3 = function () {
+  console.log("React");
+};
+
+var getname2 = () => {
+  console.log("Ankush");
+};
---
+++ 02this.js
@@ -0,0 +1,13 @@
+console.log(x);
+
+var a = 10;
+
+function b() {
+  var x = 100;
+  console.log(x);
+}
+
+console.log(window.a);
+console.log(this.a);
+
+console.log(a);
