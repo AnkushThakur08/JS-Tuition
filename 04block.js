b31a0c293c51be88bcd10f333f1012fcac66d64e
Ankush Thakur <atpara99@gmail.com>
2021-12-18 17:36:14 +0530

    Day -4

---
+++ 04block.js
@@ -0,0 +1,24 @@
+/* if (true) {
+  // Compond Statement
+  var a = 10;
+  console.log(a);
+} */
+
+let z = 100;
+z = "ankush";
+console.log(z);
+
+let b = 2000;
+{
+  var a = 1000;
+  let b = 20;
+  const c = 30;
+
+  console.log(a);
+  console.log(b);
+  console.log(c);
+}
+
+console.log(a);
+console.log(b);
+console.log(c);
